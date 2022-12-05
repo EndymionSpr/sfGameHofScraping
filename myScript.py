fa7084219b40efc31da3f9d3aec200b78b1c09b0
Jakub Drymel <endymion1@vp.pl>
2022-12-05 15:08:26 +0100

    Delete myScript.py

--- myScript.py
+++
@@ -1,6 +0,0 @@
-myList = [];
-with open("./Data/w59Test.har" , 'r') as f:
-        for line in f:
-            if 'otherplayergroupname' in line:
-                with open('Data/output', 'a') as file:
-                    file.write(line)
